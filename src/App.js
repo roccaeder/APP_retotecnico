142dedb9b4323b34bfb1c753cf2d8914e9f9a765
eder <edra1791@gmail.com>
2021-12-01 16:07:13 -0500

    add fixed any bugs

--- src/App.js
+++ src/App.js
@@ -9,25 +9,45 @@ function App() {
     let map = {
       '(': ')'
     };
-
+    console.log(str);
     if (str === "") {return SetResponse("")}
 
     for (let i = 0; i < str.length; i++) {
-        if (str[i] === '(' ) {
-          if (str[i]+str[i+1]+str[i+2] === '(:)') {return SetResponse("Balanceado")}
-          else if(str[i]+str[i+1]+str[i+2]+str[i+3] === '(:))') {return SetResponse("Balanceado")}
-          else if(str[i]+str[i+1]+str[i+2]+str[i+3] === '(:()') {return SetResponse("Balanceado")}
-            stack.push(str[i]);
+      console.log(i);
+      if (str[i] === ':' ) {
+        if (str[i]+str[i+1] === ':)') {
+          i = i+1;
+          SetResponse("Balanceado")
         }
-        else if (str[i] === ')' ) {
-            let last = stack.pop();
-            if (str[i] !== map[last]) {return SetResponse("Desbalanceado")};
+        else if (str[i]+str[i+1] === ':(') { 
+          i = i+1;
+          SetResponse("Balanceado")
         }
-        else if (str[i] === ':' ) {
-            i++;
+      }
+      else if (str[i] === '(' ) {
+        stack.push(str[i]);
+        if(str[i]+str[i+1]+str[i+2]+str[i+3] === '(:))') { 
+          i = i+3;
+          SetResponse("Balanceado")
+          stack.pop();
         }
+        else if(str[i]+str[i+1]+str[i+2]+str[i+3] === '(:()') { 
+          i = i+3;
+          SetResponse("Balanceado")
+          stack.pop();
+        }
+        else if (str[i]+str[i+1]+str[i+2] === '(:)') { 
+          i = i+2;
+          stack.pop();
+          SetResponse("Balanceado")
+        }
+      }
+      else if (str[i] === ')' ) {
+          let last = stack.pop();
+          if (str[i] !== map[last]) {return SetResponse("Desbalanceado")};
+      }
     }
-    
+    console.log(stack)
     return stack.length !== 0 ? SetResponse("Desbalanceado"):SetResponse("Balanceado");
 
   }
